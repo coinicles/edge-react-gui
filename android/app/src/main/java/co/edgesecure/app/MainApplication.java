6ca84d7fe93a7e5a85a2d68be5d23ed49b8f0b97
William Swanson <swansontec@gmail.com>
2020-06-03 10:59:34 -0700

    wip android fixes

--- android/app/src/main/java/co/edgesecure/app/MainApplication.java
+++ android/app/src/main/java/co/edgesecure/app/MainApplication.java
@@ -3,6 +3,7 @@ package co.edgesecure.app;
 import android.app.Application;
 import android.content.Context;
 import android.webkit.WebView;
+import com.bugsnag.android.Bugsnag;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -60,7 +61,7 @@ public class MainApplication extends Application implements ReactApplication {
     WebView.setWebContentsDebuggingEnabled(true);
 
     // Background task:
-    MessagesWorker.ensureScheduled(context);
+    // MessagesWorker.ensureScheduled(context);
     // MessagesWorker.testRun(context);
   }
 
